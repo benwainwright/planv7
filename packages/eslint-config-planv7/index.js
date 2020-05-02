1082082df0a06a23f9667e126df2126a1f9fa48d
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-05-02 17:23:04 +0100

    Linting rules

--- packages/eslint-config-planv7/index.js
+++ packages/eslint-config-planv7/index.js
@@ -1,8 +1,6 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
-  plugins: [
-    "@typescript-eslint", "filenames", "import", "fp" 
-  ],
+  plugins: ["@typescript-eslint", "filenames", "import", "fp"],
   extends: [
     "prettier",
     "eslint:recommended",
@@ -12,16 +10,16 @@ module.exports = {
     "prettier/@typescript-eslint",
     "plugin:import/errors",
     "plugin:import/warnings",
-    "plugin:import/typescript"
+    "plugin:import/typescript",
   ],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: "module",
   },
   rules: {
+    // Eslint build in
     "no-inline-comments": "error",
     "no-bitwise": "error",
-    "no-array-constructor": "error",
     "sort-imports": "error",
     "prefer-template": "error",
     "prefer-spread": "error",
@@ -29,8 +27,6 @@ module.exports = {
     "prefer-arrow-callback": "error",
     "object-shorthand": "error",
     "no-useless-rename": "error",
-    // For some reason ESLINT has a bug that causes a crash here
-    //"no-useless-constructor": "error",
     "no-useless-computed-key": "error",
     "no-duplicate-imports": "error",
     "prefer-object-spread": "error",
@@ -42,13 +38,15 @@ module.exports = {
     "no-multi-assign": "error",
     "no-lonely-if": "error",
     "new-cap": "error",
-    "capitalized-comments": ["error", "always", { "ignoreConsecutiveComments": true }],
-    "camelcase": "error",
+    "capitalized-comments": [
+      "error",
+      "always",
+      { ignoreConsecutiveComments: true },
+    ],
     "no-undefined": "error",
-    "no-shadow": "error", 
-    "yoda": ["error", "never"],
+    "no-shadow": "error",
+    yoda: ["error", "never"],
     "require-unicode-regexp": "error",
-    "require-await": "error",
     "prefer-regex-literals": "error",
     "prefer-promise-reject-errors": "error",
     "prefer-named-capture-group": "error",
@@ -70,11 +68,10 @@ module.exports = {
     "no-new-func": "error",
     "no-new": "error",
     "no-multi-str": "error",
-    "no-magic-numbers": ["error", { "ignore": [0] }],
     "no-loop-func": "error",
     "no-lone-blocks": "error",
     "no-labels": "error",
-    "no-iterator": 'error',
+    "no-iterator": "error",
     "no-invalid-this": "error",
     "no-implied-eval": "error",
     "no-implicit-coercion": "error",
@@ -87,25 +84,86 @@ module.exports = {
     "no-alert": "error",
     "max-classes-per-file": "error",
     "guard-for-in": "error",
-    "default-param-last": "error",
-    "complexity": "error",
+    complexity: "error",
     "no-await-in-loop": "error",
     "no-console": "error",
     "require-atomic-updates": "error",
     "array-callback-return": "error",
     "no-extra-parens": "error",
     "no-template-curly-in-string": "error",
+
+    // TypeScript
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "default",
+        format: ["camelCase"],
+        leadingUnderscore: "forbid",
+        trailingUnderscore: "forbid",
+      },
+      {
+        selector: "variable",
+        format: ["camelCase", "UPPER_CASE"],
+        leadingUnderscore: "forbid",
+        trailingUnderscore: "forbid",
+      },
+
+      {
+        selector: "typeLike",
+        format: ["PascalCase"],
+        leadingUnderscore: "forbid",
+        trailingUnderscore: "forbid",
+      },
+    ],
+    "@typescript-eslint/method-signature-style": ["error", "property"],
+
+    "@typescript-eslint/explicit-module-boundary-types": "error",
+    "@typescript-eslint/explicit-member-accessibility": "error",
+    "@typescript-eslint/class-literal-property-style": ["error", "fields"],
+    "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+    "@typescript-eslint/class-literal-property-style": ["error", "fields"],
+    "@typescript-eslint/ban-ts-comment": "error",
+    "@typescript-eslint/array-type": ["error", { default: "array" }],
+    "@typescript-eslint/no-magic-numbers": ["error", { ignore: [0] }],
     "@typescript-eslint/explicit-function-return-type": "error",
     "@typescript-eslint/no-explicit-any": "error",
     "@typescript-eslint/no-non-null-assertion": "error",
     "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-useless-constructor": "error",
+    "@typescript-eslint/array-type": "error",
+    "@typescript-eslint/default-param-last": "error",
+    "@typescript-eslint/no-array-constructor": "error",
+    "@typescript-eslint/no-parameter-properties": "error",
+    "@typescript-eslint/unified-signatures": "error",
+    "@typescript-eslint/prefer-optional-chain": "error",
+    "@typescript-eslint/prefer-for-of": "error",
+
+    // The following rules don't work; they cause eslint to crash
+    // "@typescript-eslint/no-floating-promises": "error",
+    // "@typescript-eslint/no-base-to-string": "error",
+    // "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
+    // "@typescript-eslint/no-unnecessary-condition": "error",
+    // "@typescript-eslint/restrict-plus-operands": "error",
+    // "@typescript-eslint/promise-function-async": "error",
+    // "@typescript-eslint/prefer-string-starts-ends-with": "error",
+    // "@typescript-eslint/switch-exhaustiveness-check": "error",
+    // "@typescript-eslint/no-require-imports": "error",
+    // "@typescript-eslint/prefer-nullish-coalescing": "error",
+    // "@typescript-eslint/prefer-includes": "error",
+    // "@typescript-eslint/prefer-readonly-parameter-types": "error",
+    // "@typescript-eslint/require-await": "error",
+    // "@typescript-eslint/prefer-reduce-type-parameter": "error",
+
+    // Filenames
     "filenames/match-regex": ["error", "^[a-z\\-]+(\\.[a-z]+)*$", true],
     "filenames/match-exported": "error",
-    "fp/no-let": "error"
+
+    // Fp
+    "fp/no-let": "error",
   },
   settings: {
-    "react": {
-      "version": "detect"
-    }
-  }
-}
+    react: {
+      version: "detect",
+    },
+  },
+};
