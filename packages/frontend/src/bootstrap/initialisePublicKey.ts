9c87cd0ebcfcd83db1d736b4199ff8be06659617
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-05-10 13:01:26 +0100

    Add public key

---
+++ packages/frontend/src/bootstrap/initialisePublicKey.ts
@@ -0,0 +1,11 @@
+import { TEST_PUBLIC_KEY } from "@planv7/framework";
+
+const initialisePublicKey = (): string => {
+  if (!process.env.JWT_PUBLIC_KEY && process.env.NODE_ENV === "production") {
+    throw new Error(`Must specify JWT_PUBLIC_KEY for production build`);
+  }
+
+  return process.env.JWT_PUBLIC_KEY || TEST_PUBLIC_KEY;
+};
+
+export default initialisePublicKey;
--- packages/frontend/src/renderApp.tsx
+++ packages/frontend/src/renderApp.tsx
@@ -2,11 +2,17 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import App from "./components/App";
-import { BrowserRouter } from "react-router-dom";
+import { Container } from "inversify";
 import Theme from "./components/Theme";
 import { ThemeProvider } from "@material-ui/core/styles";
+import bindDependencies from "./bootstrap/bindDependencies";
+import initialisePublicKey from "./bootstrap/initialisePublicKey";
 
-const renderApp = (): void => {
+const renderApp = async (): Promise<void> => {
+  const container = new Container();
+  const key = initialisePublicKey();
+  await bindDependencies(container, key);
+
   ReactDOM.hydrate(
     <ThemeProvider theme={Theme}>
       <App />
