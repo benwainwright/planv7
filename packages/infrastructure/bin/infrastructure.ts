afea87720bf35b5c56d92288a4283e9171df800e
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-05-11 00:40:27 +0100

    Added more inf code

--- packages/infrastructure/bin/infrastructure.ts
+++ packages/infrastructure/bin/infrastructure.ts
@@ -3,5 +3,12 @@ import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
 import ApplicationDeploymentStack from "../src/stacks/ApplicationDeploymentStack";
 
+const EU_WEST_2_LONDON_CODE_DEPLOY_RESOURCES_BUCKET =
+  "aws-codedeploy-eu-west-2";
+
 const app = new cdk.App();
-new ApplicationDeploymentStack(app, { applicationName: "Planv7" });
+
+new ApplicationDeploymentStack(app, {
+  applicationName: "Planv7",
+  codeDeployBucket: EU_WEST_2_LONDON_CODE_DEPLOY_RESOURCES_BUCKET,
+});
--- packages/infrastructure/src/stacks/ApplicationDeploymentStack.ts
+++ packages/infrastructure/src/stacks/ApplicationDeploymentStack.ts
@@ -1,9 +1,14 @@
-import * as cdk from "@aws-cdk/core";
 import * as ec2 from "@aws-cdk/aws-ec2";
+import * as cdk from "@aws-cdk/core";
+import * as path from "path";
 
 interface ApplicationDeploymentStackProps {
   applicationName: string;
+  codeDeployBucket: string;
 }
+
+const TAG_KEY = "Name";
+const TAG_VALUE = "Planv7";
 
 export default class ApplicationDeploymentStack extends cdk.Stack {
   public constructor(
@@ -13,7 +18,6 @@ export default class ApplicationDeploymentStack extends cdk.Stack {
     super(context, `${props.applicationName}DeploymentStack`);
 
     const vpc = new ec2.Vpc(this, `${props.applicationName}Vpc`);
-
     const machineImage = ec2.MachineImage.latestAmazonLinux();
 
     const instanceType = ec2.InstanceType.of(
@@ -21,11 +25,28 @@ export default class ApplicationDeploymentStack extends cdk.Stack {
       ec2.InstanceSize.MICRO
     );
 
-    new ec2.Instance(this, `${props.applicationName}Instance`, {
-      instanceType,
-      machineImage,
-      vpc,
-      instanceName: `${props.applicationName}Instance`,
-    });
+    const userData = `#!/bin/bash
+yum -y update
+yum install -y ruby
+cd /home/ec2-user
+curl -O https://${props.codeDeployBucket}.s3.amazonaws.com/latest/install
+chmod +x ./install
+./install auto`;
+
+    const instance = new ec2.Instance(
+      this,
+      `${props.applicationName}Instance`,
+      {
+        instanceType,
+        machineImage,
+        vpc,
+        userData: ec2.UserData.custom(userData),
+        instanceName: `${props.applicationName}Instance`,
+      }
+    );
+
+    instance.connections.allowDefaultPortFromAnyIpv4();
+
+    cdk.Tag.add(instance, TAG_KEY, TAG_VALUE);
   }
 }
