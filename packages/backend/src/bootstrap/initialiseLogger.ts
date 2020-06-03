126eaded148adf10a7cae95a560271e7e6154be7
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-06-03 20:59:21 +0100

    Add logfile

--- packages/backend/src/bootstrap/initialiseLogger.ts
+++ packages/backend/src/bootstrap/initialiseLogger.ts
@@ -1,19 +1,27 @@
+import * as constants from "../constants";
 import { TYPES as APP, Logger } from "@choirpractise/application";
 import { WinstonConfig, WinstonLogger } from "@choirpractise/framework";
 import { format, transports } from "winston";
 import { Container } from "inversify";
 
 const initialiseLogger = (container: Container): Logger => {
-  const loggingFormat =
-    process.env.NODE_ENV !== "production"
-      ? format.combine(format.timestamp(), format.colorize())
-      : format.combine(format.simple());
+  const transportConfig =
+    process.env.NODE_ENV === "production"
+      ? [
+          new transports.Console({
+            format: format.simple(),
+          }),
+          new transports.File({
+            filename: `/var/log/${constants.APP_NAME}/app.log`,
+          }),
+        ]
+      : [
+          new transports.Console({
+            format: format.colorize(),
+          }),
+        ];
 
-  const loggingConfig = new WinstonConfig(process.env.APP_LOG_LEVEL ?? "info", [
-    new transports.Console({
-      format: loggingFormat,
-    }),
-  ]);
+  const loggingConfig = new WinstonConfig(process.env.APP_LOG_LEVEL ?? "info", transportConfig);
 
   container.bind<WinstonConfig>(WinstonConfig).toConstantValue(loggingConfig);
   container.bind<Logger>(APP.logger).to(WinstonLogger);
