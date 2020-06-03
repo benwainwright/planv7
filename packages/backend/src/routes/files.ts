7e65f10a96fa9fbd79178dacc7b235de95a1391c
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-06-03 21:03:35 +0100

    Add logging

--- packages/backend/src/routes/files.ts
+++ packages/backend/src/routes/files.ts
@@ -1,5 +1,6 @@
 import * as AWS from "aws-sdk";
 import * as constants from "../constants";
+import { TYPES as APP, Logger } from "@choirpractise/application";
 import Koa, { Next } from "koa";
 import Router, { RouterContext } from "koa-router";
 import AppContext from "../AppContext";
@@ -16,24 +17,29 @@ const files = (): Koa.Middleware<any, any> => {
   router.post(
     baseUrlRegex,
     async (context: AppContext & RouterContext, next: Next) => {
-      if (!context.request.body.path) {
-        context.response.status = 400;
-        context.response.body = "Missing 'path' parameter";
-      } else if (!context.request.body.contentType) {
-        context.response.status = 400;
-        context.response.body = "Missing 'contentType' parameter";
-      } else {
-        const options = {
-          /* eslint-disable @typescript-eslint/naming-convention */
-          Bucket: process.env.FILES_BUCKET,
-          ContentType: context.request.body.contentType,
-          Key: context.request.body.path,
-          Expires: constants.PRESIGNED_URL_EXPIRY,
-          /* eslint-enable @typescript-eslint/naming-convention */
+      const logger = context.container.get<Logger>(APP.logger);
+      try {
+        if (!context.request.body.path) {
+          context.response.status = 400;
+          context.response.body = "Missing 'path' parameter";
+        } else if (!context.request.body.contentType) {
+          context.response.status = 400;
+          context.response.body = "Missing 'contentType' parameter";
+        } else {
+          const options = {
+            /* eslint-disable @typescript-eslint/naming-convention */
+            Bucket: process.env.FILES_BUCKET,
+            ContentType: context.request.body.contentType,
+            Key: context.request.body.path,
+            Expires: constants.PRESIGNED_URL_EXPIRY,
+            /* eslint-enable @typescript-eslint/naming-convention */
+          }
+          const url = await s3.getSignedUrlPromise("putObject", options);
+          context.response.status = 200;
+          context.response.body = JSON.stringify({ url });
         }
-        const url = await s3.getSignedUrlPromise("putObject", options);
-        context.response.status = 200;
-        context.response.body = JSON.stringify({ url });
+      } catch (error) {
+        logger.error(error);
       }
       await next();
     }
